4016f629ce387bc8c644a7d9a15db4000bc2818b
Justin Self <justn.self@gmail.com>
2014-02-20 15:35:18 -0600

    added convert controller and blank view

---
+++ AsciiIt.Web/Controllers/ConvertController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AsciiIt.Web.Controllers
+{
+    public class ConvertController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Index(HttpPostedFile image)
+        {
+            return Json("Success");
+        }
+    }
+}
